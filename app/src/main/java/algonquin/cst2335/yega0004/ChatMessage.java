73c1f78b65b43683ff825955887cc48126f3315f
ImanYeganeh <imanyegane30@gmail.com>
2023-11-15 16:43:43 -0500

    Finsihing the Lab 7 connecting the android app to database :)

--- app/src/main/java/algonquin/cst2335/yega0004/ChatMessage.java
+++ app/src/main/java/algonquin/cst2335/yega0004/ChatMessage.java
@@ -1,25 +1,46 @@
-package algonquin.cst2335.yega0004;public class ChatMessage {
+    package algonquin.cst2335.yega0004;
 
-    public String message;
-    public String timeSent;
-    public boolean isSentButton;
+    import androidx.room.ColumnInfo;
+    import androidx.room.Entity;
+    import androidx.room.PrimaryKey;
 
-    public ChatMessage (String m, String t, boolean sent)
-    {
-        message = m;
-        timeSent = t;
-        isSentButton = sent;
+
+    @Entity
+    public class ChatMessage {
+
+
+        @ColumnInfo(name="message")
+        public String message;
+
+        @ColumnInfo(name="TimeSent")
+        public String timeSent;
+
+        @ColumnInfo(name="IsSentButton")
+        public boolean isSentButton;
+
+        @PrimaryKey (autoGenerate = true)
+        @ColumnInfo(name="id")
+        public int id;
+
+
+        public ChatMessage() {
+        }
+        public ChatMessage (String m, String t, boolean sent)
+        {
+            message = m;
+            timeSent = t;
+            isSentButton = sent;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getTimeSent() {
+            return timeSent;
+        }
+
+        public boolean isSentButton() {
+            return isSentButton;
+        }
     }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getTimeSent() {
-        return timeSent;
-    }
-
-    public boolean isSentButton() {
-        return isSentButton;
-    }
-}
---
+++ app/src/main/java/algonquin/cst2335/yega0004/ChatMessageDAO.java
@@ -0,0 +1,23 @@
+package algonquin.cst2335.yega0004;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+
+@Dao
+public interface ChatMessageDAO {
+    @Insert
+    public long insertMessage(ChatMessage m);
+
+    @Query("SELECT * FROM ChatMessage")
+    public List<ChatMessage> getAllMessages();
+
+    @Delete
+    void deleteMessage(ChatMessage m);
+
+
+}
